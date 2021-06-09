6bf41c3dbc7dda18406c0fdd91f706bb0638865e
luispi010204 <luigi.jackson9@gmail.com>
2021-06-09 12:21:19 +0200

    commit

--- src/main/java/com/example/NBA_Shop/service/TestService.java
+++ src/main/java/com/example/NBA_Shop/service/TestService.java
@@ -5,6 +5,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * service for testings
@@ -24,6 +28,24 @@ public class TestService {
         return Response
                 .status(200)
                 .entity("Erfolgreich")
+                .build();
+    }
+
+    @GET
+    @Path("restore")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response restore() {
+        try {
+            byte[] jsonData = Files.readAllBytes(Paths.get(Config.getProperty("backupJSON")));
+
+            FileOutputStream fileOutputStream = new FileOutputStream(Config.getProperty("spielerJSON"));
+            fileOutputStream.write(jsonData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Response
+                .status(200)
+                .entity("Daten wiederhergestellt")
                 .build();
     }
 }
